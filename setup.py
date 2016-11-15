5f078eba7acf11c403f45c22c7aef58a52307164
sxf <sunxfancy@gmail.com>
2016-11-15 09:48:28 +0800

    change setup.py add template

--- setup.py
+++ setup.py
@@ -5,25 +5,29 @@
 # @Date:   2016-11-14
 # @Email:  sunxfancy@gmail.com
 # @Last modified by:   sxf
-# @Last modified time: 2016-11-14
+# @Last modified time: 2016-11-15
 # @License: MIT License
 
 from setuptools import setup, find_packages
 
 setup(
-      name='codefactory',   #名称
-      version='0.10',  #版本
-      description="a native code builder using git, github service and cmake", #描述
-      keywords='code factory builder cmake github',
-      author='sxf',  #作者
-      author_email='sunxfancy@gmail.com', #作者邮箱
-      url='https://github.com/sunxfancy', #作者链接
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      py_modules=['codefactory'],
-      install_requires=[      #需求的第三方模块
-        # 'requests',
+    name='codefactory',   #名称
+    version='0.2.4',  #版本
+    description="a native code builder using git, github service and cmake", #描述
+    keywords='codef code factory builder cmake github',
+    author='sxf',  #作者
+    author_email='sunxfancy@gmail.com', #作者邮箱
+    url='https://github.com/sunxfancy', #作者链接
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'autotest']),
+    zip_safe=True,
+    data_files=[('share/codefactory/templates/', ['templates/CPPTemplate.zip'])],
+    install_requires=[      #需求的第三方模块
         'click'
-      ]
+    ],
+    entry_points={
+        'console_scripts':[
+            'codef = codefactory.codef:cli'
+        ]
+    }
 )
