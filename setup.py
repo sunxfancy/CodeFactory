3a599a5ff64a362e1f887b21fdcee86be9d23ab3
西风逍遥游 <sunxfancy@gmail.com>
2017-03-18 09:10:54 +0800

    change to py2 and py3 compatible code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codefactory',   #名称
-    version='0.3.1',  #版本
+    version='0.4.3',  #版本
     description="a native code builder using git, github service and cmake", #描述
     keywords='codef code factory builder cmake github',
     author='sxf',  #作者
@@ -23,7 +23,6 @@ setup(
     zip_safe=True,
     data_files=[('share/codefactory/templates/', ['templates/CPPTemplate.zip'])],
     install_requires=[      #需求的第三方模块
-        'click'
     ],
     entry_points={
         'console_scripts':[
